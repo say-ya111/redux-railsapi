aa7c6980c7e643a29c9bb3d7f23c14380e4e8a11
watanabe saiya <say-ya111@users.noreply.github.com>
2019-10-17 01:18:02 +0900

    fix userReducer

--- front_end/modules/Reducers.ts
+++ front_end/modules/Reducers.ts
@@ -7,7 +7,7 @@ import { connectRouter } from 'connected-react-router'
 const initialState = {
   userState: {
     name: '',
-    isLoggedIn: true
+    isLoggedIn: false
   },
   counterState: [
     {
